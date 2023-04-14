eba5e421b40da1ba40c3a3f0a62228a46d7f36da
Leo-Cabrera <kanomody29@gmail.com>
2023-04-14 13:28:03 +0200

    cagada

---
+++ Practica 2/Practica02_ej10/Practica02_ej10/Form1.cs
@@ -0,0 +1,25 @@
+namespace Practica02_ej10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void C0_0_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void C0_1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void C0_2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

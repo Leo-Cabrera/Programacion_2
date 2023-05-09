e313b69c2a381f59d7aa3b48d0a8d03e9b514ec4
Leo-Cabrera <kanomody29@gmail.com>
2023-05-09 19:16:32 +0200

    clase 09/05

--- pruebas/pruebas/Program.cs
+++ pruebas/pruebas/Program.cs
@@ -8,13 +8,99 @@ namespace pruebas
 {
     internal class Program
     {
+        class ListaNodos
+        {
+            public Nodo[] listanodos { get; }
+            public bool IsEmpty()
+            {
+                bool vacia = false;
+                foreach ( Nodo i in listanodos)
+                {
+                    if(i == null)
+                    {
+                        vacia = true;
+                    }
+                
+                }
+                return vacia;
+            }
+            public void Add(Object valor)
+            {
+               
+          
+            }
+            public bool Contain (Object valor)
+            {
+                bool contiene = false;
+                foreach (Nodo i in listanodos)
+                {
+                    if (i.Dato == valor)
+                    {
+                        contiene = true;
+                    }
+
+                }
+                return contiene;
+
+            }
+            public void Remove (object valor)
+            {
+                foreach (Nodo i in listanodos)
+                {
+                    if (i.Dato == valor)
+                    {
+                        i.Dato = null;
+                    }
+
+                }
+                foreach (Nodo i in listanodos)
+                {
+                    if (i.Dato == null)
+                    {
+                        Nodo aux = i;
+                        Nodo nodo1 = i;
+                        Nodo nodo2 = i.Next;
+                    }
+
+                }
+            }
+            public Object ElementAt(int indice)
+            {
+                if (indice>=0 && indice < listanodos.Length)
+                {
+                    return listanodos[indice];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            public override string ToString()
+            {
+              for(i)
+            }
+        }
+        class Nodo
+        {
+            
+            public Nodo Sig { get; set; }
+            public Object Dato { get; set; }
+            public Nodo(Object dato)
+            {
+                this.Dato = dato;
+                this.Sig = null;
+            }
+        }
         static void Main(string[] args)
         {
-            int[] a = { 1, 2 };
+            Nodo n1 = new Nodo("awimawe");
+            Nodo n2 = new Nodo("awimawe");
+            Nodo n3 = new Nodo("uuuuuuuuuuuuuuuuu");
+            n1.Sig = n2;
+            n2.Sig = n3;
 
-            int b = Convert.ToInt32(a);
-            Console.WriteLine(b);
-            Console.ReadKey();
+
+
         }
     }
 }
